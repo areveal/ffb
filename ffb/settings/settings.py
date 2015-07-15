cbe0f5f48b2625ca23dcfb5c33b995e9c5ece151
Alexi Cole Reveal <alexi.reveal@gmail.com>
2015-07-14 22:23:39 -0500

    Modify Database for Project
    This project should point to a local mysql database.

--- ffb/settings/settings.py
+++ ffb/settings/settings.py
@@ -40,7 +40,7 @@ DEFAULT_APPS = (
 )
 
 THIRD_PARTY_APPS = (
-
+    'south',
 )
 
 LOCAL_APPS = (
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ffb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'local_ffb_test_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
