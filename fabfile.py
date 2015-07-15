4478bab5574f7e95ccfe22e6b6672b4321058e98
Alexi Cole Reveal <alexi.reveal@gmail.com>
2015-07-14 22:47:01 -0500

    Add FabFile
    Adding the fab file to the project for deployment purposes.

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import lcd, local
+
+
+def prepare_deployment(branch_name):
+    local('python manage.py test django_project')
+    local('git add -p && git commit') # or local('hg add && hg commit')
+
+
+def deploy():
+    with lcd('/path/to/my/prod/area/'):
+
+        # With git...
+        local('git pull /my/path/to/dev/area/')
+
+        # With both
+        local('python manage.py migrate myapp')
+        local('python manage.py test myapp')
+        local('/my/command/to/restart/webserver')
--- ffb/settings/__init__.py
+++ ffb/settings/__init__.py
@@ -1,1 +1,3 @@
 __author__ = 'reveal'
+
+from settings import *
