5af2c3a0b9208f6b6e2a27f0629fe9262d74e7da
Alexi Cole Reveal <alexi.reveal@gmail.com>
2015-07-19 13:07:29 -0500

    Initial commit for Player model

--- ffb/players/models.py
+++ ffb/players/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    nflgame_id = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    first_name = models.CharField(max_length=50, null=True, blank=True)
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    college = models.CharField(max_length=50, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    height = models.IntegerField(null=True, blank=True, default=0)
+    weight = models.IntegerField(null=True, blank=True, default=0)
+    position = models.CharField(max_length=50, null=True, blank=True)
+    team = models.CharField(max_length=4, null=True, blank=True)
